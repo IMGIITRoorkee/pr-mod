f0d9f9d3f5d371f5e562787407333a83a8c37fee
Dragneel7 <sainisurya.1@gmail.com>
2018-12-19 23:18:33 +0530

    add comments to functions

--- pr-mod/views.py
+++ pr-mod/views.py
@@ -1,41 +1,56 @@
 import os
 
-from flask import session, redirect
+from flask import redirect, url_for, session, request
 from git import Repo
 from app import app, github
 from config import Config
 
-@app.route('/')
+
+@app.route('/', methods=['POST'])
 def index():
-    if session.get('token') is None:
-        return redirect('/authorize')
-    
-    # Start a DIND enviroment before pull 
-    return redirect('/git_pull')
+    """ Redirects to authorize a user/org.
+    """
+    session['remote_url'] = request.args.get('remote')
+    return redirect('/authorize')
+
 
 @app.route('/git_pull')
 def git_pull_repo():
+    """ Pulls a repo from github.
+    :returns: Url for the deployed application.
+    :rtype: string  
+    """
     https_remote_url = 'https://{0}:x-oauth-basic@{1}'.format(
         session.get('token'),
-        # repo remote URL
-    )
+        session.get('remote_url')
+        )
     repo_name = '<repo_name>'
     cloned_repo = Repo.clone_from(https_remote_url, repo_name)
-    
     # generate url for deployed app
     return '<app_url>'
 
+
 @app.route('/authorize')
-def login():
+def authorize():
+    """ Authorizes user with defined scope.
+    """
     return github.authorize(scope=Config.scope)
+
 
 @app.route('/callback')
 @github.authorized_handler
-def authorized(oauth_token):
+def authorization_callback(oauth_token):
+    """ Callback function after user authorization.
+    :param access_token: Authorization token for the logged in user.
+    :type access_token: string
+    :returns: Redirects to git-pull function.  
+    """
     if oauth_token is None:
         return "User Not Authenticated"
     session['token'] = oauth_token
-    return redirect('/')
+    # Start a DIND enviroment before pull 
+    return redirect('/git_pull')
+
 
 if __name__ == '__main__':
     app.secret_key = Config.SECRET_KEY
